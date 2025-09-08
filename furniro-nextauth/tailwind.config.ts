9c7cc319ea5bbe6c56786f7226fbbe77dfaa42b9
abdullahN111 <162550451+abdullahN111@users.noreply.github.com>
2025-09-08 22:54:48 +0500

    made changes in tailwind config file

--- furniro-nextauth/tailwind.config.ts
+++ furniro-nextauth/tailwind.config.ts
@@ -1,12 +1,12 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-
   ],
   theme: {
     extend: {
@@ -14,11 +14,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
-      
-     
+
       screens: {
-            "xsx": "480px"
-          }
+        xsx: "480px",
+      },
     },
   },
   plugins: [],
